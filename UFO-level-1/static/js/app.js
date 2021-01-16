159130664a0e63e75e250edb3976c9c905e2205c
monica <monicaalina58@gmail.com>
2021-01-15 19:09:07 -0500

    UFO-LEVEL-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,45 @@
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+var tableData = data;
+var tbody = d3.select("tbody");
+
+
+// Refactor to use Arrow Functions
+data.forEach((ufo) => {
+    
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(([key, value]) => {        
+        var cell = row.append("td");
+        cell.text(value);
+    });
+  });
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Create event handlers 
+button.on("click", function(){
+    tbody.html("");
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
+    // Display the filteredData
+    filteredData.forEach((ufo) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+    d3.event.preventDefault();
+});
